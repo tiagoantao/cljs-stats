7419f21b439192791c21570932d909cd3cbd0a74
Tiago Antao <tiagoantao@gmail.com>
2017-09-04 17:11:24 -0600

    Pearson correlation

--- src/stats/test.cljs
+++ src/stats/test.cljs
@@ -1,7 +1,11 @@
 (ns stats.test
   "Statistical Tests
   "
-
+  (:require [stats.stats :refer std]
+            [stats.multi :refer cov])
   {:author "Tiago Antao"}
-)
+  )
 
+(defn pearson [v1 v2]
+  (/ (cov v1 v2)
+     (* (std v1) (std v2))))

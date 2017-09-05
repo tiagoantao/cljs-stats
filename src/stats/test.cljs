20d5ca50a25a0c2aad01968a23b514bde35c06dc
Tiago Antao <tiagoantao@gmail.com>
2017-09-04 20:28:27 -0600

    spearman: ongoing

--- src/stats/test.cljs
+++ src/stats/test.cljs
@@ -9,3 +9,24 @@
 (defn pearson [v1 v2]
   (/ (cov v1 v2)
      (* (std v1) (std v2))))
+
+
+(defn all-pos [v coll]
+  (let [first (some #(= v %) coll)]
+    (if (nil? first)
+      (list)
+      XXXX
+     ) 
+    ))
+
+(defn rank [v]
+  (let [s (sort v)]))
+
+
+(defn rank-sp
+  "Spearman rank correlation"
+  [v1 v2]
+  (let [r1 (rank v1)
+        r2 (rank v2)]
+    (pearson r1 r2))
+  )

c92643f285636c76431fd5e953d40e5c538ee5e7
Tiago Antao <tiagoantao@gmail.com>
2017-09-08 13:24:24 -0600

    test nobs on lists and vecs

--- test/stats/sample_test.cljs
+++ test/stats/sample_test.cljs
@@ -7,6 +7,11 @@
   (testing "Basic sample testing"
     (is (=
          (seq (s/Sample. {:hist {1 2 2 3}}))
-         '(1 1 2 2 2)))))
+         '(1 1 2 2 2))))
+  (testing "nobs works for vectors and lists"
+    (is (= (s/nobs [1 2 3]) 3))
+    (is (= (s/nobs '(1 2 3)) 3)))
+
+  )
 
 

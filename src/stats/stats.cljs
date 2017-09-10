bc22514d08bc8b300d2d01bb44844a109b556946
Tiago Antao <tiagoantao@gmail.com>
2017-09-09 18:27:08 -0600

    population variance and stddev

--- src/stats/stats.cljs
+++ src/stats/stats.cljs
@@ -9,12 +9,24 @@
   [v] (/ (reduce + (seq v)) (nobs v)))
 ;This seq should be removed
 
+(defn mean1
+  "Mean but divided by obs - 1"
+  [v] (/ (reduce + (seq v)) (dec (nobs v))))
+;This seq should be removed
+
 (defn variance
+  [v] (let [mean-v (mean v)]
+        (mean1 (map #(.pow js/Math (- % mean-v) 2) v))))
+
+(defn pop-variance
   [v] (let [mean-v (mean v)]
         (mean (map #(.pow js/Math (- % mean-v) 2) v))))
 
 (defn std
   [v] (js/Math.sqrt (variance v)))
+
+(defn pop-std
+  [v] (js/Math.sqrt (pop-variance v)))
 
 (defn median
   "Median"
--- test/stats/stats_test.cljs
+++ test/stats/stats_test.cljs
@@ -11,10 +11,25 @@
   ))
 
 
+(deftest mean1
+  (testing "Mean but divided by obs - 1"
+    (is (= (s/mean1 '(1 2)) 3))
+  ))
+
+
 (deftest median
   (testing "Median"
     (is (= (s/median (Sample. {:obs [1 2 3 4 5 6]})) 3.5))
-    (is (= (s/median '(1 2 3 4 5) 3)))
+    (is (= (s/median '(1 2 3 4 5)) 3))
     ))
 
+(deftest variance
+  (testing "Variance"
+    (is (= 2.5 (s/variance [1 2 3 4 5])))
+    (is (= 0 (s/variance [1 1])))
+    ))
 
+(deftest pop-variance
+  (testing "Population variance"
+    (is (= 2 (s/pop-variance [1 2 3 4 5])))
+    ))
